01332fed2573f24edc2103c12bc1b6e972e8fccb
Pannawich <6810110200@psu.ac.th>
2025-12-16 16:22:25 +0700

    3rd-checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import NoteForm from './component/NoteForm';
 import NoteList from './component/NoteList';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import type { Note } from './types';
 import './App.css'; 
 
 function App() {
-
   const [notes, setNotes] = useState<Note[]>([]);
+  // 1. เพิ่มตัวแปรเช็คว่าโหลดข้อมูลเสร็จหรือยัง
+  const [isLoading, setIsLoading] = useState(true);
+
+  // 2. Load Data (ทำงานครั้งแรกครั้งเดียว)
+  useEffect(() => {
+    const saved = localStorage.getItem('notes');
+    if (saved) {
+      setNotes(JSON.parse(saved) as Note[]);
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  // 3. Save Data (ทำงานเมื่อ notes เปลี่ยนแปลง)
+  useEffect(() => {
+    // เช็คว่าถ้าโหลดเสร็จแล้ว ค่อยบันทึก
+    if (!isLoading) {
+      localStorage.setItem('notes', JSON.stringify(notes));
+    }
+  }, [notes, isLoading]); // เพิ่ม isLoading 
+
   const addNote = (text: string) => {
-  const newNote: Note = {
-    id: Date.now(), 
-    text: text,
+    const newNote: Note = {
+      id: Date.now(), 
+      text: text,
     };
     setNotes([...notes, newNote]);
+  };
+
+  const deleteNote = (id: number) => {
+    setNotes(notes.filter((note) => note.id !== id));
   };
 
   return (
     <div>
       <NoteForm onAdd={addNote}/>
-      <NoteList notes={notes}/>
+      <NoteList notes={notes} onDelete={deleteNote}/>
     </div>
   );
 }
--- src/component/NoteList.tsx
+++ src/component/NoteList.tsx
@@ -1,13 +1,19 @@
 import type{ Note } from '../types';
 
-interface NoteListProps { notes: Note[]; }
+interface NoteListProps { notes: Note[]; onDelete: (id: number) => void;}
 
-const NoteList = ({ notes }: NoteListProps) => {
+
+const NoteList = ({ notes, onDelete }: NoteListProps) => {
   return (
     <ul>
       {notes.map((note) => (
         <li key={note.id}>
           {note.text}
+          <button 
+            onClick={() => onDelete(note.id)} 
+            style={{ marginLeft: '10px', background: '#ff4444', color: 'white', border: 'none', padding: '5px 10px', borderRadius: '4px', cursor: 'pointer' }}>
+            Delete
+          </button>
         </li>
       ))}
     </ul>
